38c12c2dd7726701128682381665cae1ff1071c1
Leondeodas1 <leondeodas1@gmail.com>
2023-02-25 19:21:01 -0500

    added airconditions and style

--- flask_app/static/javascript/search.js
+++ flask_app/static/javascript/search.js
@@ -1,6 +1,7 @@
-thisis = document.querySelector("#info")
-box = document.getElementById('hi')
-getid = document.querySelector("#forest")
+var thisis = document.querySelector("#info")
+var box = document.getElementById('hi')
+var getid = document.querySelector("#forest")
+var airquaity = document.querySelector("#air")
 console.log(getid)
 var currentSearch = "";
 
@@ -14,10 +15,11 @@ function makecodercard(data) {
     var res = `
     <div id="info" style= "border-style: solid;width:50%;border-radius: 5%;padding: 10px;">
                     <p style="font-weight: bolder; font-size: x-large;"> ${currentSearch}</p>
+                    <p style="font-size: small;">tempature ${data.temp_f}</p>
                     <p>Chance of rain ${data.precip_in}</p>
                     <p>${data.cloud}</p>
                     <img src="${data.condition.icon}" alt="${data.condition.icon}">
-                    <p style="font-size: small;">tempature ${data.temp_f}</p>
+                    
                     <p style="font-size: small;"> humidity: ${data.humidity} </p>
                 
                 
@@ -28,52 +30,38 @@ function makecodercard(data) {
 function forecastcard(data) {
     var fore = `
     <div style="display: flex; justify-content: space-evenly">
-            <div id="forest" style= "border-style: solid;width:16%;border-radius: 5%;padding: 10px;">
-                <p >todays forcast</p>
-                <p>Chance of rain ${data.hour[6].precip_in}</p>
-                <p>${data.hour[6]}</p>
+            <div id="forest" style= "border-right: solid ; width:16%;padding: 10px;">
+                <p >6:00 AM</p>
                 <img src="${data.hour[6].condition.icon}" alt="${data.hour[6].condition.icon}">
                 <p style="font-size: small;">tempature ${data.hour[6].temp_f}</p>
                 <p style="font-size: small;"> humidity: ${data.hour[6].humidity} </p>
-                        
-                        
             </div>
-            <div id="forest" style= "border-style: solid;width:16%;border-radius: 5%;padding: 10px;">
-                <p >todays forcast</p>
-                <p>Chance of rain ${data.hour[9].precip_in}</p>
-                <p>${data.hour[9]}</p>
+            <div id="forest" style= "border-right: solid ;width:16%;padding: 10px;">
+                <p >9:00 AM</p>
                 <img src="${data.hour[9].condition.icon}" alt="${data.hour[9].condition.icon}">
                 <p style="font-size: small;">tempature ${data.hour[9].temp_f}</p>
                 <p style="font-size: small;"> humidity: ${data.hour[9].humidity} </p>
             </div>
-            <div id="forest" style= "border-style: solid;width:16%;border-radius: 5%;padding: 10px;">
-                <p >todays forcast</p>
-                <p>Chance of rain ${data.hour[12].precip_in}</p>
-                <p>${data.hour[12]}</p>
+            <div id="forest" style= "border-right: solid ;width:16%;padding: 10px;">
+                <p >12:00 PM</p>
                 <img src="${data.hour[12].condition.icon}" alt="${data.hour[12].condition.icon}">
                 <p style="font-size: small;">tempature ${data.hour[12].temp_f}</p>
                 <p style="font-size: small;"> humidity: ${data.hour[12].humidity} </p>
             </div>
-            <div id="forest" style= "border-style: solid;width:16%;border-radius: 5%;padding: 10px;">
-                <p >todays forcast</p>
-                <p>Chance of rain ${data.hour[3].precip_in}</p>
-                <p>${data.hour[3]}</p>
+            <div id="forest" style= "border-right: solid ;width:16%;;padding: 10px;">
+                <p >3:00 PM</p>
                 <img src="${data.hour[3].condition.icon}" alt="${data.hour[3].condition.icon}">
                 <p style="font-size: small;">tempature ${data.hour[3].temp_f}</p>
                 <p style="font-size: small;"> humidity: ${data.hour[3].humidity} </p>
             </div>
-            <div id="forest" style= "border-style: solid;width:16%;border-radius: 5%;padding: 10px;">
-                <p >todays forcast</p>
-                <p>Chance of rain ${data.hour[6].precip_in}</p>
-                <p>${data.hour[6]}</p>
+            <div id="forest" style= "border-right: solid ;width:16%;padding: 10px;">
+                <p >6:00 PM</p>
                 <img src="${data.hour[6].condition.icon}" alt="${data.hour[6].condition.icon}">
                 <p style="font-size: small;">tempature ${data.hour[6].temp_f}</p>
                 <p style="font-size: small;"> humidity: ${data.hour[6].humidity} </p>
             </div>
-            <div id="forest" style= "border-style: solid;width:16%;border-radius: 5%;padding: 10px;">
-                <p >todays forcast</p>
-                <p>Chance of rain ${data.hour[9].precip_in}</p>
-                <p>${data.hour[9]}</p>
+            <div id="forest" style= "width:16%;padding: 10px;">
+                <p >9:00 PM</p>
                 <img src="${data.hour[9].condition.icon}" alt="${data.hour[9].condition.icon}">
                 <p style="font-size: small;">tempature ${data.hour[9].temp_f}</p>
                 <p style="font-size: small;"> humidity: ${data.hour[9].humidity} </p>
@@ -82,11 +70,26 @@ function forecastcard(data) {
         
     return fore;
 }
+function airqua(data) {
+    var res = `
+    <div id="info" style= "border-style: solid;width:50%;border-radius: 5%;padding: 10px;">
+                    <p style="font-weight: bolder; font-size: x-large;"> Air conditions</p>
+                    <p style="font-size: small;">real feel ${data.feelslike_f}</p>
+                    <p>Chance of rain ${data.precip_in} %</p>
+                    <p>${data.wind_mph} mph</p>
+                    <p style="font-size: small;"> UV index: ${data.uv} </p>
+                
+                
+        </div>`
+        
+    return res;
+}
 async function show() {
     var response = await fetch("http://api.weatherapi.com/v1/forecast.json?key=eff73a53fac3473e8e1182249232502&q=" + currentSearch + "&aqi=no");
     var coderData = await response.json();
     console.log(coderData)
     thisis.innerHTML = makecodercard(coderData.current);
     getid.innerHTML = forecastcard(coderData.forecast.forecastday[0]);
+    airquaity.innerHTML = airqua(coderData.current);
 }
 
