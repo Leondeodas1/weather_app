f357605f9cab86e06826407a33293d0397ab0614
Leondeodas1 <leondeodas1@gmail.com>
2023-03-01 13:08:51 -0500

    adding time

--- flask_app/static/javascript/darkmode.js
+++ flask_app/static/javascript/darkmode.js
@@ -1,16 +1,33 @@
+
+
+
 var bodystyle = document.querySelector("body")
 
 function changecolor(element){
-    bodystyle.style.background = "#222222";
-    bodystyle.style.color = "#ffffff";
-    element.innerText = "off"
+    if (element.innerText == "dark Mode"){
+        bodystyle.style.background = "#222222";
+        bodystyle.style.color = "#ffffff";
+        element.innerText = "lightmode"
+    }
+    if (element.innerText == "lightmode"){
+        bodystyle.style.background = "#333333";
+    }
+    var listoftime = ['1PM', '2PM', '3PM', '4PM', '5PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM', '12PM', '1AM', '2AM', '3AM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM'];
+    
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+      
+    // Check whether AM or PM
+    var newformat = hours >= 12 ? 'PM' : 'AM';
+      
+    // Find current hour in AM-PM Format
+    hours = hours % 12;
+      
+    // To display "0" as "12"
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    something = hours +  newformat;
+    console.log(something)
+    console.log(typeof(something))
 }
-
-// var today = new Date();
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// var dateTime = time;
- 
-// console.log(dateTime)
-
-
-
--- flask_app/static/javascript/search.js
+++ flask_app/static/javascript/search.js
@@ -206,34 +206,34 @@ const errorCallback = (error) => {
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 
-// var xyValues = [
-//     {x:5, y:7},
-//     {x:60, y:8},
-//     {x:70, y:8},
-//     {x:80, y:9},
-//     {x:90, y:9},
-//     {x:100, y:9},
-//     {x:110, y:10},
-//     {x:120, y:11},
-//     {x:130, y:14},
-//     {x:140, y:14},
-//     {x:150, y:15}
-//   ];
+var xyValues = [
+    {x:5, y:7},
+    {x:60, y:8},
+    {x:70, y:8},
+    {x:80, y:9},
+    {x:90, y:9},
+    {x:100, y:9},
+    {x:110, y:10},
+    {x:120, y:11},
+    {x:130, y:14},
+    {x:140, y:14},
+    {x:150, y:15}
+  ];
   
-//   new Chart("myChart", {
-//     type: "scatter",
-//     data: {
-//       datasets: [{
-//         pointRadius: 4,
-//         pointBackgroundColor: "rgb(0,0,255)",
-//         data: xyValues
-//       }]
-//     },
-//     options: {
-//       legend: {display: false},
-//       scales: {
-//         xAxes: [{ticks: {min: 1, max:24}}],
-//         yAxes: [{ticks: {min: 1, max:43}}],
-//       }
-//     }
-//   });
+  new Chart("myChart", {
+    type: "scatter",
+    data: {
+      datasets: [{
+        pointRadius: 4,
+        pointBackgroundColor: "rgb(0,0,255)",
+        data: xyValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        xAxes: [{ticks: {min: 1, max:24}}],
+        yAxes: [{ticks: {min: 1, max:43}}],
+      }
+    }
+  });
