92ace5ee9e00cf12392bb4fd0e3c4dd54407ecbb
Leondeodas1 <leondeodas1@gmail.com>
2023-02-28 23:58:50 -0500

    darkmode

---
+++ flask_app/static/javascript/darkmode.js
@@ -0,0 +1,16 @@
+var bodystyle = document.querySelector("body")
+
+function changecolor(element){
+    bodystyle.style.background = "#222222";
+    bodystyle.style.color = "#ffffff";
+    element.innerText = "off"
+}
+
+// var today = new Date();
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = time;
+ 
+// console.log(dateTime)
+
+
+
--- flask_app/static/javascript/exampletime.js
+++ flask_app/static/javascript/exampletime.js
@@ -17,3 +17,26 @@ const convertTime = timeStr => {
    return `${hours}`;
 };
         console.log(convertTime(timeStr));
+
+
+
+            // var date = new Date();
+    // var hours = date.getHours();
+    // var minutes = date.getMinutes();
+      
+    // // Check whether AM or PM
+      
+    // // Find current hour in AM-PM Format
+    // hours = hours % 12;
+      
+    // // To display "0" as "12"
+    // hours = hours ? hours : 12;
+    // minutes = minutes < 10 ? '0' + minutes : minutes;
+    // something = hours + ':' + minutes + ' ' + newformat;
+    // console.log(something)
+    // if(something <= "7:00 PM"){
+    //     bodystyle.style.background = "#222222";
+    //     bodystyle.style.color = "#ffffff";
+    // }else{
+    //     console.log("no")
+    // }
